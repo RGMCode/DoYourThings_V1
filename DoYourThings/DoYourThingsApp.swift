13aa89ffdfa9bcccf079dba4a59342ed0566f92f
RGMCode <coding@mengedoth.net>
2024-05-26 21:21:25 +0200

    Initial Commit

---
+++ DoYourThings/DoYourThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoYourThingsApp.swift
+//  DoYourThings
+//
+//  Created by RGMCode on 26.05.24.
+//
+
+import SwiftUI
+
+@main
+struct DoYourThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
